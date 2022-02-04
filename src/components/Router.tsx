736b63cf8d99c454cca3ae69b5cbcac6df975c1c
김범창 <gimbeomchang@gimbeomchangs-Mac-mini.local>
2022-02-04 21:27:33 +0900

    2.5 Log Out

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import Auth from '../routes/Auth';
-import Home from '../routes/Home';
+import Auth from 'routes/Auth';
+import Profile from 'routes/Profile';
+import Home from 'routes/Home';
+import Navigation from './Navigation';
 
 export interface AppRouterProps {
 	isLoggedIn: boolean;
@@ -12,11 +14,17 @@ const AppRouter = (props: AppRouterProps) => {
 
 	return (
 		<Router>
+			{isLoggedIn && <Navigation />}
+
 			<Switch>
 				{isLoggedIn ? (
 					<>
 						<Route exact path="/">
 							<Home />
+						</Route>
+
+						<Route exact path="/profile">
+							<Profile />
 						</Route>
 					</>
 				) : (
